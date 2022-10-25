5b47a8bb2400694520e703a419abb1743daf48f5
thomassssilva <thomasceval1234@hotmail.com>
2022-10-25 16:53:59 -0300

    git modified all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,7 @@
 //MINHA RESOLUÇÃO
+function formatZeros(num) { return num >= 10 ? num : `0${num}` }
+const data = new Date();
 
-function formatZeros (num) {
-    return num >=10 ? num : `0${num}`
-}
-
-const data = new Date ();
 const diaSemana = data.getDay();
 const dia = formatZeros(data.getDate());
 const mes = data.getMonth();
@@ -14,37 +11,13 @@ const minuto = formatZeros(data.getMinutes());
 const segundo = formatZeros(data.getSeconds());
 const ms = data.getMilliseconds();
 
-const diaSemanaTxt = [
-    'Domingo',
-    'Segunda-Feira',
-    'Terça-Feira',
-    'Quarta-Feira',
-    'Quinta-Feira',
-    'Sexta-Feira'
-];
-
-const mesTxt = [
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-    'Agosto',
-    'Setembro',
-    'Outubro',
-    'Novembro',
-    'Dezembro',
-];
-
+const diaSemanaTxt = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira'];
+const mesTxt = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro',];
 
 const texto = `Hoje é ${diaSemanaTxt[diaSemana]}, ${dia} de ${mesTxt[mes]} de ${ano}, e são ${hora}h ${minuto}m ${segundo}.${ms}s`;
 
 const resultado = document.querySelector('#resultado');
 resultado.innerHTML = texto;
-
-console.log(texto);
 
 /*//SOLUÇÃO PROFESSOR 1
 const data = new Date ();
